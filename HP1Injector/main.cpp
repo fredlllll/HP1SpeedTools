4433b41131011a1890742382798981c97e55b125
Frederik Gelder <frederik.gelder@freenet.de>
2017-07-27 23:48:18 +0200

    added waiting for the softdrv.dll to be loaded

--- HP1Injector/main.cpp
+++ HP1Injector/main.cpp
@@ -6,13 +6,28 @@
 
 using namespace std;
 
-void startProcessInject(char* exePath, char* dllPath) {
+void startProcessAndInject(char* exePath, char* dllPath) {
 	STARTUPINFO si;
 	PROCESS_INFORMATION pi;
 	ZeroMemory(&si, sizeof(STARTUPINFO));
 	ZeroMemory(&pi, sizeof(PROCESS_INFORMATION));
 	si.cb = sizeof(STARTUPINFO);
+
+	//either with detours
 	DetourCreateProcessWithDll(NULL, exePath, NULL, NULL, FALSE, CREATE_DEFAULT_ERROR_MODE, NULL, NULL, &si, &pi, dllPath, NULL);
+
+	//or with createremotethread. both have the same result
+	/*
+	if (CreateProcess(NULL, exePath, NULL, NULL, FALSE, CREATE_DEFAULT_ERROR_MODE, NULL, NULL, &si, &pi)) {
+		LPVOID LoadLibraryAddr = (LPVOID)GetProcAddress(GetModuleHandle("kernel32.dll"), "LoadLibraryA");
+		LPVOID LoadLibraryParam = (LPVOID)VirtualAllocEx(pi.hProcess, NULL, strlen(dllPath), MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE); //allocating memory in other process
+		WriteProcessMemory(pi.hProcess, LoadLibraryParam, dllPath, strlen(dllPath), NULL); //writing dll path there
+		CreateRemoteThread(pi.hProcess, NULL, NULL, (LPTHREAD_START_ROUTINE)LoadLibraryAddr, LoadLibraryParam, NULL, NULL); //calling load library using remote thread
+		VirtualFreeEx(pi.hProcess, LoadLibraryParam, 0, MEM_RELEASE); //free memory after calling loadLibrary
+		CloseHandle(pi.hProcess);
+		CloseHandle(pi.hThread);
+	}
+	*/
 }
 
 int main(int argc, char** argv) {
@@ -21,10 +36,10 @@ int main(int argc, char** argv) {
 		unique_ptr<char> DetourPath(new char[MAX_PATH]);
 		GetCurrentDirectory(MAX_PATH, DirPath.get());
 		sprintf_s(DetourPath.get(), MAX_PATH, "%s\\HP1SpeedTools.dll", DirPath.get());
-		startProcessInject(argv[1], DetourPath.get());
+		startProcessAndInject(argv[1], DetourPath.get());
 	}
 	else {
-		cout << "please specify the path to the HP1.exe as argument (for now, will make dynamic later)";
+		cout << "please specify the path to the HP1.exe (with arguments) as argument (for now, will make dynamic later)";
 	}
 	return 0;
 }
--- HP1SpeedTools/main.cpp
+++ HP1SpeedTools/main.cpp
@@ -2,6 +2,12 @@
 
 #include <Windows.h>
 #include <detours.h>
+#include <thread>
+
+class FPlane {
+public:
+	float x, y, z, w;
+};
 
 class _USoftwareRenderDevice {
 public:
@@ -9,44 +15,63 @@ public:
 	{
 		return 0;
 	}
+
+	virtual void __thiscall Lock(FPlane x, FPlane y, FPlane z, unsigned long a, unsigned char *b, int *c) {
+		MessageBox(0, "yaaay lock called", "o boi", 0);
+	}
 };
 
-typedef int (__thiscall _USoftwareRenderDevice::*execFuncPtr_t)(unsigned short const* something, void* outputDevice);
+typedef int(__thiscall _USoftwareRenderDevice::*execFuncPtr_t)(unsigned short const* something, void* outputDevice);
 struct pointerHolder {
 	execFuncPtr_t fp;
 };
 
-
-PVOID detour_execFuncPtr;
-PVOID origin_execFuncPtr;
+//pointer to detour function
+PVOID detourFP;
 
 HMODULE hDLL;
 
-void attach(){
-	DisableThreadLibraryCalls(hDLL);
-	DetourTransactionBegin();
-	DetourUpdateThread(GetCurrentThread());
-	DetourAttach(&(PVOID&)origin_execFuncPtr, detour_execFuncPtr);
-	if (DetourTransactionCommit() == NO_ERROR) {
-		OutputDebugString("send() detoured successfully");
+DWORD WINAPI attachWaiter(LPVOID param) {
+	//wait till SoftDrv.dll is loaded
+
+	while (true) {
+		std::chrono::milliseconds delay(1000);
+		std::this_thread::sleep_for(delay);
+
+		HMODULE moduleHandle = GetModuleHandle("SoftDrv.dll");
+		if (moduleHandle != NULL) {
+
+			FARPROC originalFP = GetProcAddress(moduleHandle, "?Lock@USoftwareRenderDevice@@UAEXVFPlane@@00KPAEPAH@Z"); //will it work? *drumroll* no clue yet lol
+			DetourTransactionBegin();
+			DetourUpdateThread(GetCurrentThread());
+			DetourAttach(&(PVOID&)originalFP, detourFP);
+			if (DetourTransactionCommit() == NO_ERROR) {
+				OutputDebugString("send() detoured successfully");
+			}
+			break;
+		}
 	}
+	return 0;
+}
+
+
+void attach() {
+	DisableThreadLibraryCalls(hDLL); //to prevent deadlock (i think, but cant hurt)
+	CreateThread(NULL, 0, attachWaiter, NULL, 0, NULL);
 }
 
 void detach() {
-	DetourTransactionBegin();
+	//lets just ignore that for now. we would have to sync with the attachWaiter thread, and i dont even intend on detaching this dll
+	/*DetourTransactionBegin();
 	DetourUpdateThread(GetCurrentThread());
-	DetourDetach(&(PVOID&)origin_execFuncPtr, detour_execFuncPtr);
-	DetourTransactionCommit();
-}
-
-void setOriginalFP() {
-	origin_execFuncPtr = (PVOID)0x444;
+	DetourDetach(&(PVOID&)origin_execFuncPtr, detourFP);
+	DetourTransactionCommit();*/
 }
 
 void setDetourFP() {
 	pointerHolder ph;
 	ph.fp = &_USoftwareRenderDevice::Exec;
-	detour_execFuncPtr = (PVOID)*((intptr_t*)&ph); //convert to intptr_t* and dereference that (to get fp as int)
+	detourFP = (PVOID)*((intptr_t*)&ph); //convert to intptr_t* and dereference that (to get fp as int)
 }
 
 INT APIENTRY DllMain(HMODULE hDLL, DWORD Reason, LPVOID Reserved)
@@ -54,7 +79,6 @@ INT APIENTRY DllMain(HMODULE hDLL, DWORD Reason, LPVOID Reserved)
 	switch (Reason)
 	{
 	case DLL_PROCESS_ATTACH:
-		setOriginalFP();
 		setDetourFP();
 
 		::hDLL = hDLL;
@@ -62,6 +86,7 @@ INT APIENTRY DllMain(HMODULE hDLL, DWORD Reason, LPVOID Reserved)
 		break;
 	case DLL_PROCESS_DETACH:
 		detach();
+		break;
 	case DLL_THREAD_ATTACH:
 	case DLL_THREAD_DETACH:
 		break;
